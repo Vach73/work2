e15ba3245137ba3244d73084c0d0efbfc7ac870b
Vach73 <109985576+Vach73@users.noreply.github.com>
2022-07-26 13:21:32 +0400

    work2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  //   direction: "vertical",
+  //   loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-controlers-next",
+    prevEl: ".slider-controlers-prev",
+  },
+});
